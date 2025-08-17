a747981c3cb83ed225672878f698dff8fa87f040
microUranium <nano6123micro@gmail.com>
2025-08-17 11:44:55 +0900

    敵の拡充

--- scripts/systems/TitleScreen.gd
+++ scripts/systems/TitleScreen.gd
@@ -18,7 +18,7 @@ func _unhandled_input(event: InputEvent) -> void:
 
 func _start_stage_with_random_seed() -> void:
   """ランダムシードでステージ開始"""
-  var pool_sequence := [{"pool": "stage1", "count": 10}, {"pool": "stage1_boss", "count": 1}]
+  var pool_sequence := [{"pool": "stage2", "count": 10}, {"pool": "stage2_boss", "count": 1}]
 
   var random_seed := RandomSeedGenerator.generate_seed_with_pools(pool_sequence)
   print("TitleScreen: Generated random seed: %s" % random_seed)
@@ -28,7 +28,7 @@ func _start_stage_with_random_seed() -> void:
 func _on_switch_to_s1_intro_pressed() -> void:
   """S1イントロボタン - 固定シード設定"""
   #var fixed_seed := "Ds1d11.intro-s2g1-s221-s211-s111-s112-s131-s1g1-Ds1d11.progression-s121-s1g1-s112-s141-s112-s1z1-Ds1d11.resolution"
-  var fixed_seed := "s211-s221-s231-s2z3-s2z2-Ds2d12.resolution"
+  var fixed_seed := "Ds2d11.intro-s231-s211-s241-s2g1-s231-s211-s221-Ds2d11.progression-s2z1-Ds2d11.resolution"
   RandomSeedGenerator.set_current_seed(fixed_seed)
   print("TitleScreen: Set fixed intro seed: %s" % fixed_seed)
   GameFlow.start_stage()
@@ -36,7 +36,8 @@ func _on_switch_to_s1_intro_pressed() -> void:
 
 func _on_switch_to_s1_boss_pressed() -> void:
   """S1ボスボタン - 固定シード設定"""
-  var fixed_seed := "Ds1d12.intro-s113-s122-s1g3-s113-s142-Ds1d12.progression-s134-s1g2-s1g4-s1g3-s1g5-s1z3-s1z2-Ds1d12.resolution"
+  #var fixed_seed := "Ds1d12.intro-s113-s122-s1g3-s113-s142-Ds1d12.progression-s134-s1g2-s1g4-s1g3-s1g5-s1z3-s1z2-Ds1d12.resolution"
+  var fixed_seed := "s213-s2g7-s234-s2g6-s213-s2g9-s2ga-s2g8-s2gb-s2z3-s2z2-Ds2d12.resolution"
   RandomSeedGenerator.set_current_seed(fixed_seed)
   print("TitleScreen: Set fixed boss seed: %s" % fixed_seed)
   GameFlow.start_stage()
